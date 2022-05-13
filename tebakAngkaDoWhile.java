9478d50f81328e114be750d0b02f5d331dc491b8
JamaludinSeptian <jamaludinseptian180@gmail.com>
2022-05-13 20:18:24 +0700

    Menambahkan fitur uang

--- tebakAngkaDoWhile.java
+++ tebakAngkaDoWhile.java
@@ -13,9 +13,12 @@ public class tebakAngka {
             int min = 1;
             int benar = 0;
             int salah = 0;
+            int balanced;
+            int total;
             int range = max - min + 1;
             System.out.println("SELAMAT DATANG DI TEBAK ANGKA");
             System.out.println("=================================");
+            System.out.print("Masukan angka taruhan minimal 50.000 : "); balanced=sc.nextInt();
             do{
                 System.out.println("");
                 System.out.println("Ini adalah percobaan ke - "+percobaan); 
@@ -23,18 +26,26 @@ public class tebakAngka {
                 int rand = (int)(Math.random() * range) + min;
                 if(angka == rand){
                     System.out.println("Tebakan anda benar, yaitu angka - "+rand);
+                    balanced += 10000;
                     benar++;
                 }else{
                     System.out.println("Tebakan anda salah, yang benar adalah angka - "+rand);
+                    balanced -= 10000;
                     salah++;
                 }
-                
-                System.out.print("Apakah anda ingin mencoba lagi (Y/T) ? "); pilihan=sc.next();
+                System.out.println("Uang anda adalah "+balanced);
+                if(balanced > 0){
+                    System.out.print("Apakah anda ingin mencoba lagi (Y/T) ? "); pilihan=sc.next();
+                }else{
+                    System.out.println("");
+                    System.out.print("Maaf uang anda habis tidak bisa melakukan taruhan");
+                }
                 System.out.println("");
                 percobaan++;
-            }while(pilihan.equalsIgnoreCase("y"));
-            System.out.println("Tebakan benar anda adalah "+benar);
-            System.out.println("Tebakan salah anda adalah "+salah);
+            }while(pilihan.equalsIgnoreCase("y") && balanced > 0);
+            System.out.println("");
+            System.out.println("Tebakan benar adalah "+benar);
+            System.out.println("Tebakan salah adalah "+salah);
             System.out.println("Terima kasih");
         }catch(Exception e){
             System.out.println("Error : "+e.getMessage());
